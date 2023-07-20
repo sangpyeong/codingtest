9c022740e4836bcba92abf4692de2b803f3379c8
sangpyeong <miasp1105@naver.com>
2023-07-20 14:59:52 +0900

    7/20

---
+++ 프로그래머스/표현 가능한 이진트리.py
@@ -0,0 +1,26 @@
+# 정답
+def search(number):
+    length = len(number)
+    if length == 1 or '1' not in number or '0' not in number:
+        return True
+
+    mid = length // 2
+    if number[mid] == '0':
+        return False
+
+    return search(number[:mid]) and search(number[mid+1:])
+
+
+def solution(numbers):
+    bin_numbers = [bin(x)[2:] for x in numbers]
+    bin_list = [2**x - 1 for x in range(50)]
+    answer = list()
+    for number in bin_numbers:
+        length = len(number)
+        for num in bin_list:
+            if num >= length:
+                number = '0'*(num-length) + number
+                break
+        answer.append(1 if search(number) else 0)
+
+    return answer
