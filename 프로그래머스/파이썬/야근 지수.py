3e2646433cf6eefe249b60d1b57d337101fc9e96
sangpyeong <miasp1105@naver.com>
2023-09-07 18:30:11 +0900

    9/7

---
+++ 프로그래머스/파이썬/야근 지수.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+def solution(n, works):
+    answer = 0
+    hq = []
+    for i in range(len(works)):
+        heapq.heappush(hq, works[i] * (-1))
+    for i in range(n):
+        if not hq:
+            break
+        tmp = heapq.heappop(hq)
+        tmp += 1
+        if tmp < 0:
+            heapq.heappush(hq, tmp)
+    while hq:
+        tmp = heapq.heappop(hq)
+        answer += tmp * tmp
+    return answer
