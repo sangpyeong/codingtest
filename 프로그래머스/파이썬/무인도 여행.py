7a491f3e68a73059701e0ef43289920ee956b279
sangpyeong <miasp1105@naver.com>
2023-11-28 19:23:33 +0900

    11/28

--- 프로그래머스/파이썬/무인도 여행.py
+++ 프로그래머스/파이썬/무인도 여행.py
@@ -1,34 +1,34 @@
 from collections import deque
 
-dx = [1, 0 ,-1, 0]#상 우 하 좌
+dx = [1, 0, -1, 0]  # 상 우 하 좌
 dy = [0, 1, 0, -1]
-def calculate_day(map, x, y):
-    result=0
+
+
+def calculate_day(map, x, y, visited):
+    result = 0
     q = deque([(x, y)])
-    visited=[]
-    while(q):
+    visited[x][y] = True
+    while (q):
         tx, ty = q.popleft()
         result += int(map[tx][ty])
-        map[tx] = list(map[tx])
-        map[tx][ty] = "X"
-        map[tx] = ''.join(s for s in map[tx])
         for i in range(4):
             nx = tx + dx[i]
             ny = ty + dy[i]
-            if nx>=0 and nx <len(map) and ny>=0 and ny<len(map[0]) and map[nx][ny]!="X" and (nx, ny) not in visited:
-                visited.append((nx,ny))
-                q.append((nx,ny))
+            if nx >= 0 and nx < len(map) and ny >= 0 and ny < len(map[0]) and map[nx][ny] != "X" and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny))
     return result
-    
-    
+
+
 def solution(maps):
     answer = []
+    visited = [[False]*len(maps[0]) for _ in range(len(maps))]
     for i in range(len(maps)):
         for j in range(len(maps[0])):
-            if maps[i][j] == "X":
+            if maps[i][j] == "X" or visited[i][j]:
                 continue
-            answer.append(calculate_day(maps, i, j))
+            answer.append(calculate_day(maps, i, j, visited))
     answer.sort()
-    if len(answer)==0:
+    if len(answer) == 0:
         answer = [-1]
     return answer
--- 프로그래머스/파이썬/방문 길이.py
+++ 프로그래머스/파이썬/방문 길이.py
@@ -1,48 +1,62 @@
+# def solution(dirs):
+#     answer = 0
+#     #좌표
+#     maps = [[""]*11 for _ in range(11)]
+#     #이동
+#     start = [5,5]
+#     for i in range(len(dirs)):
+#         if dirs[i] == 'U' and start[0]>=1:
+#             if "U" not in maps[start[0]][start[1]]:
+#                 maps[start[0]][start[1]] += "U"
+#                 answer+=1
+#                 start[0] -= 1
+#                 maps[start[0]][start[1]] += "D"
+#             else:
+#                 start[0] -= 1
+
+#         elif dirs[i] == 'D' and start[0]<=9:
+#             if "D" not in maps[start[0]][start[1]]:
+#                 maps[start[0]][start[1]] += "D"
+#                 answer+=1
+#                 start[0] += 1
+#                 maps[start[0]][start[1]] += "U"
+#             else:
+#                 start[0] += 1
+
+#         elif dirs[i] == 'R' and start[1]<=9:
+#             if "R" not in maps[start[0]][start[1]]:
+#                 maps[start[0]][start[1]] += "R"
+#                 answer+=1
+#                 start[1] += 1
+#                 maps[start[0]][start[1]] += "L"
+#             else:
+#                 start[1] += 1
+
+#         elif dirs[i] == 'L' and start[1]>=1:
+#             if "L" not in maps[start[0]][start[1]]:
+#                 maps[start[0]][start[1]] += "L"
+#                 answer+=1
+#                 start[1] -= 1
+#                 maps[start[0]][start[1]] += "R"
+#             else:
+#                 start[1] -= 1
+
+
+#     return answer
+
+
 def solution(dirs):
-    answer = 0
-    #좌표
-    maps = [[""]*11 for _ in range(11)]    
-    #이동
-    start = [5,5]
-    for i in range(len(dirs)):
-        if dirs[i] == 'U' and start[0]>=1:
-            if "U" not in maps[start[0]][start[1]]:
-                maps[start[0]][start[1]] += "U"
-                answer+=1
-                start[0] -= 1
-                maps[start[0]][start[1]] += "D"
-            else:
-                start[0] -= 1
- 
-        elif dirs[i] == 'D' and start[0]<=9:
-            if "D" not in maps[start[0]][start[1]]:
-                maps[start[0]][start[1]] += "D"
-                answer+=1
-                start[0] += 1
-                maps[start[0]][start[1]] += "U"
-            else:
-                start[0] += 1
-                
-        elif dirs[i] == 'R' and start[1]<=9:
-            if "R" not in maps[start[0]][start[1]]:
-                maps[start[0]][start[1]] += "R"
-                answer+=1
-                start[1] += 1
-                maps[start[0]][start[1]] += "L"
-            else:
-                start[1] += 1
-                
-        elif dirs[i] == 'L' and start[1]>=1:
-            if "L" not in maps[start[0]][start[1]]:
-                maps[start[0]][start[1]] += "L"
-                answer+=1
-                start[1] -= 1
-                maps[start[0]][start[1]] += "R"
-            else:
-                start[1] -= 1
-        
-                    
-    
-    return answer
+    sets = set()
+    y, x = 0, 0
+    udrl = {'U': (1, 0), 'D': (-1, 0), 'R': (0, 1), 'L': (0, -1)}
 
-
+    for d in dirs:
+        dy, dx = udrl[d]
+        ny = y + dy
+        nx = x + dx
+        if -5 <= ny <= 5 and -5 <= nx <= 5:
+            sets.add(((y, x), (ny, nx)))
+            sets.add(((ny, nx), (y, x)))
+            y = ny
+            x = nx
+    return len(sets) // 2
