a5495876e998ad43a444a6b7eb612564eb98e2bf
sangpyeong <miasp1105@naver.com>
2023-12-13 16:13:13 +0900

    12/13

--- 프로그래머스/파이썬/세 소수의 합.py
+++ 프로그래머스/파이썬/세 소수의 합.py
@@ -1,17 +1,24 @@
-from itertools import combinations
-
-
 def solution(n):
     sieve = [True] * n
     for i in range(2, int(n ** 0.5) + 1):
-        print(i)
         if sieve[i]:
             for j in range(i * 2, n, i):
                 sieve[j] = False
 
     prime_numbers = [i for i in range(2, n) if sieve[i]]
-    l = [sum(c) for c in combinations(prime_numbers, 3)]
-    return [sum(c) for c in combinations(prime_numbers, 3)].count(n)
+    cb = []
+
+    def combi(start, list):
+        if len(list) == 3:
+            if sum(list) == n:
+                cb.append(list)
+            return
+        for i in range(start, len(prime_numbers)):
+            combi(i+1, list + [prime_numbers[i]])
+
+    combi(0, [])
+    print(cb)
+    return len(cb)
 
 
-solution(33)
+print(solution(33))
