6999f1bb6c4c5de5829f29d5f24836f10000e424
sangpyeong <miasp1105@naver.com>
2023-12-19 18:42:23 +0900

    12/19

--- 프로그래머스/파이썬/스킬트리.py
+++ 프로그래머스/파이썬/스킬트리.py
@@ -1,12 +1,14 @@
 def solution(skill, skill_trees):
     answer = 0
 
-    for skill_tree in skill_trees:
+    for skills in skill_trees:
         skill_list = list(skill)
-        for s in skill_tree:
-            if s in skill and s != skill_list.pop(0):
-                break
-        else:  # for-else 문법을 사용하면 dirty flag를 제거할 수 있고 파이썬스럽게 푼 느낌이 든다.
+
+        for s in skills:
+            if s in skill:
+                if s != skill_list.pop(0):
+                    break
+        else:
             answer += 1
 
     return answer
