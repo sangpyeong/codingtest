e2a2e098193097e580f96791c13523f96effe0f0
sangpyeong <miasp1105@naver.com>
2023-12-22 18:58:46 +0900

    12/22

--- 프로그래머스/파이썬/스타 수열.py
+++ 프로그래머스/파이썬/스타 수열.py
@@ -1,62 +1,24 @@
-def solution(a):
-    answer = -1
-    #스타 수열이려면 같은 숫자가 많아야하고 연속되면 안됨, a의 길이는 500000까지, 부분수열 
-    #해를 구하는 방법 -> 연속이 아닌경우 그냥 증가, 연속인 경우 앞뒤 한칸식은 달라야하고 
-    numbers = [0] * len(a)
-    for i in range(len(a)):
-        if len(a) > i + 1 and a[i] == a[i+1]:
-            if i + 1 == len(a) -1 or i == 0:
-                continue
-            elif i + 2 < len(a) and a[i+1] == a[i+2]:
-                continue
-            elif i + 3 == len(a) - 1 and a[i+1] == a[i+3]:
-                continue
-            elif i + 4 < len(a) and a[i+1] == a[i+3] and a[i+1] == a[i+4]:
-                continue
-        
-        else:
-            numbers[a[i]] += 1
-            
-    max_number = max(numbers)
-    
-    if len(a) < 2:
-        answer = 0
-    else:
-        answer = max_number * 2
-    
-        
-    return answer
-
-#정답
 from collections import Counter
 
+
 def solution(a):
-    answer = -1
+    if len(a) < 2:
+        return 0
 
-    # 현재 배열에 숫자들이 나온 횟수들
-    els = Counter(a)
-    # a에 있는 각 원소 k를 기준으로 스타배열을 만들 수 있는지 검사
-    for k in els.keys():
+    counter = Counter(a)
+    counter = sorted(counter.items(), key=lambda x: x[1], reverse=True)
 
-        # 현재 k의 등장횟수가 스타수열에 사용된 공통 인자 횟수 이하면 continue
-        if els[k] <= answer:
-            continue
-    
-        # k의 등장 횟수
-        cnt = 0
-        idx = 0
-        while idx < len(a)-1:
-            # 두 칸 모두 k가 포함 안되어있거나 두 칸이 같은 값이면 스타수열 안되니까 continue
-            if (a[idx] != k and a[idx+1] != k) or (a[idx] == a[idx+1]):
-                idx += 1
+    max_length = 0
+    for num, _ in counter:
+        if counter[0][1] * 2 <= max_length:
+            break
+        length = 0
+        i = 0
+        while i < len(a) - 1:
+            if (a[i] != num and a[i+1] != num) or (a[i] == a[i+1]):
+                i += 1
                 continue
-
-            # 스타수열의 원소로 추가할 수 있는 경우 k사용횟수 1 증가
-            cnt += 1
-            # 다음 배열 탐색을 위해 두칸 점프
-            idx += 2
-
-        # 스타 수열 완성에 쓰인 공통 원소 k가 사용된 최대 횟수 갱신
-        answer = max(cnt, answer)
-
-    return -1 if answer == -1 else answer*2
+            length += 2
+            i += 2
+        max_length = max(max_length, length)
+    return max_length
