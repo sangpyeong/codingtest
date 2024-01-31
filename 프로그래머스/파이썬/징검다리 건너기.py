404e4f098595a4b0f0f42487a175d67f3187db0b
sangpyeong <miasp1105@naver.com>
2024-01-31 15:59:59 +0900

    1/31

--- 프로그래머스/파이썬/징검다리 건너기.py
+++ 프로그래머스/파이썬/징검다리 건너기.py
@@ -21,22 +21,25 @@ def solution(stones, k):
 
 
 def solution(stones, k):
-    left = 1
-    right = 200000000
-    while left <= right:
-        temp = stones.copy()
-        mid = (left + right) // 2
-        cnt = 0
-        for t in temp:
-            if t - mid <= 0:
-                cnt += 1
-            else:
-                cnt = 0
-            if cnt >= k:
-                break
-        if cnt >= k:
-            right = mid - 1
-        else:
-            left = mid + 1
+    answer = 0
+    s = 1
+    e = max(stones)
 
-    return left
+    while s <= e:
+        mid = (s+e)//2
+        print(s, e, mid)
+        count = 0
+        for stone in stones:
+            if stone < mid:
+                count += 1
+            else:
+                count = 0
+            if count >= k:
+                break
+        if count >= k:
+            e = mid - 1
+        else:
+            answer = mid
+            s = mid + 1
+
+    return answer
